30176badd0cbef04427df304a3fa26ee143bf3c6
rsa1986 <rodrigo.sagostinho@gmail.com>
2026-05-25 21:20:11 -0400

    variáveis de ambiente

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://cycxfdhcfxjsrikwqknw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5Y3hmZGhjZnhqc3Jpa3dxa253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NDc2NjMsImV4cCI6MjA5NTMyMzY2M30.bn2wIPn_NKyN6Ufv9EiiswDZKmKOKx3VT1FGUNr0HTo";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
